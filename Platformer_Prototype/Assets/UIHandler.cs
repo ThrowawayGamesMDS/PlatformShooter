677fe80e0a13c9c9e549506fcb481bfa5566cdba
MDS\benjamin.poi6329 <the343guy@hotmail.com>
2018-07-16 17:26:04 +1200

    Everything I've Done Today
    UIHandler, Weapon Stats, and a Weapon handler which is all interlinked.

---
+++ Platformer_Prototype/Assets/UIHandler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour
+{
+    /// <summary>
+    /// THE DISPLAY THAT THE UI HANDLER IS ADJUSTING! IMPORTANT ASF
+    /// </summary>
+    public enum Handling
+    {
+        WEAPON, PLAYER
+    }
+    public Handling m_eObjective;
+    /// <summary>
+    /// DISPLAY 1,2,3,4 TEXTS AND UPDATE THEM IF THE MULTITEXT BOOL ISNT ACTIVE..
+    /// </summary>
+    public Text m_tDisplay1;
+    public Text m_tDisplay2;
+    public Text m_tDisplay3;
+    public Text m_tDisplay4;
+    public Text[] m_tDisplay;
+    /// <summary>
+    ///  ENTER THIS SHIT WHEN THERES MULTIPLE TEXTS LIKE MORE THAN THREE...
+    /// </summary>
+    public bool m_bMultiText;
+
+    /// <summary>
+    /// SOME STUFF FOR WEAPON UI MEM UPDATING TO SAVE INGAME PROCESSING
+    /// </summary>
+    private string m_sCurrentWeapon;
+	// Use this for initialization
+	void Start ()
+    {
+		if (m_eObjective == Handling.WEAPON)
+        {
+            m_sCurrentWeapon = WeaponHandler.CurrentGun();
+            m_tDisplay1.text = "Weapon: " + m_sCurrentWeapon;
+            m_tDisplay2.text = "Ammo: " + WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_iMagCount;
+            m_tDisplay3.text = "Weapon Level: " + WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_iWeaponLevel;
+            m_tDisplay4.text = "Weapon EXP: " + WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_fWeaponEXP;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (m_bMultiText)
+        {
+
+        }
+        else
+        {
+            switch(m_eObjective)
+            {
+                case Handling.WEAPON:
+                    {
+                        if (m_sCurrentWeapon != WeaponHandler.CurrentGun())
+                        {
+                            m_tDisplay1.text = "Weapon: " + WeaponHandler.CurrentGun();
+                        }
+                        m_tDisplay2.text = "Ammo: " + WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_iMagCount;
+                        break;
+                    }
+                default:
+                        break;
+            }
+        }
+		
+	}
+}
---
+++ Platformer_Prototype/Assets/WeaponStats.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats : MonoBehaviour
+{
+    public enum WeaponType
+    {
+        SINGLE, BURST, AUTO
+    }
+
+    public float m_fWeaponPower;
+    public int m_iMagazineCapacity;
+    public int m_iWeaponLevel;
+    public WeaponType m_eWeaponType;
+    public float m_fWeaponEXP;
+    public float m_fPower;
+    public int m_iMagCap;
+    public string m_sWeaponType;
+    public int m_iMagCount;
+	// Use this for initialization
+	void Start ()
+    {
+        m_fPower = m_fWeaponPower;
+        m_iMagCap = m_iMagazineCapacity;
+        m_sWeaponType = DetermineType();
+        m_iMagCount = m_iMagCap;
+        //m_fWeaponEXP = 0.0f;
+        //m_iWeaponLevel = 1;
+    }
+	
+    private string DetermineType()
+    {
+        var _s = "";
+        switch(m_eWeaponType)
+        {
+            case WeaponType.SINGLE:
+                {
+                    _s = "SINGLE";
+                    break;
+                }
+            case WeaponType.BURST:
+                {
+                    _s = "BURST";
+                    break;
+                }
+            case WeaponType.AUTO:
+                {
+                    _s =  "AUTO";
+                    break;
+                }
+        }
+        return _s;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+}
