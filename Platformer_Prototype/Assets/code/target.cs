e1453bffa35700d5162875e9d80b06799f8641ff
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2018-07-16 16:07:42 +1200

    folder stuction add all basic platforms add

---
+++ Platformer_Prototype/Assets/code/target.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour {
+    public GameObject platform;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            platform.GetComponent<targetPlatformMove>().stopped = false;
+            Destroy(gameObject);
+        }
+
+    }
+    void OnCollisionEnter(Collision Col)
+    {
+
+        if (Col.gameObject.tag == "Player")
+        {
+            platform.GetComponent<targetPlatformMove>().stopped = false;
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Platformer_Prototype/Assets/code/targetPlatformMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetPlatformMove : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject[] place;
+    public bool stopped = true;
+    public int speed = 5;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stopped == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, place[0].transform.position, speed * Time.deltaTime);
+
+        }
+    }
+}
