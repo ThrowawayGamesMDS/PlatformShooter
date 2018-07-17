fde3f5be84bc78c591359bef968e659e9a2ff5b6
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2018-07-17 15:41:31 +1200

    small changes to fall

--- Platformer_Prototype/Assets/code/fallPlatform.cs
+++ Platformer_Prototype/Assets/code/fallPlatform.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class fallPlatform : MonoBehaviour {
     bool Falling = false;
     public float time = 60;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    bool playerIsOn = false;
+    player play;
+    // Use this for initialization
+    void Start () {
+        play = FindObjectOfType<player>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,6 +20,10 @@ public class fallPlatform : MonoBehaviour {
             {
                 var move = new Vector3(0, -1, 0);
                 transform.position += move * 2.5f * Time.deltaTime;
+                if (playerIsOn == true)
+                {
+                    play.transform.position += move * 2.5f * Time.deltaTime;
+                }
             }
             else {
                 time--;
@@ -35,5 +41,25 @@ public class fallPlatform : MonoBehaviour {
             Falling = true;
         }
 
+    }
+    void OnCollisionStay(Collision Col)
+    {
+        if (Col.gameObject.tag == "Player")
+        {
+            playerIsOn = true;
+            //Col.getcompo
+            print("yes");
+            //Col.transform.position = Vector3.MoveTowards(Col.transform.position, place[aim].transform.position, speed * Time.deltaTime);
+        }
+    }
+    void OnCollisionExit(Collision Col)
+    {
+        if (Col.gameObject.tag == "Player")
+        {
+            play.ExtraVel = Vector3.zero;
+            playerIsOn = false;
+            print("no");
+            //Col.transform.position = Vector3.MoveTowards(Col.transform.position, place[aim].transform.position, speed * Time.deltaTime);
+        }
     }
 }
