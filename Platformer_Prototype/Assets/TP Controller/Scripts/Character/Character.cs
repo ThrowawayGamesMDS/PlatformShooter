326e98675509deceace3fd8d651f42f60484ac8a
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2018-07-24 14:26:48 +1200

    double jump has be fixed. (set to 3) checkpoint fix

--- Platformer_Prototype/Assets/TP Controller/Scripts/Character/Character.cs
+++ Platformer_Prototype/Assets/TP Controller/Scripts/Character/Character.cs
@@ -31,6 +31,7 @@ public class Character : MonoBehaviour
     public Vector3 impact = Vector3.forward;
     public int ExtraJump;
     public int maxJump;
+    public Vector3 savepoint;
    // public Transform savehome;
     #region Unity Methods
 
