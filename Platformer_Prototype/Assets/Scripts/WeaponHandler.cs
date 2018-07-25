15e94adb4502edb2317e59da308985450577be35
MDS\benjamin.poi6329 <the343guy@hotmail.com>
2018-07-25 14:58:34 +1200

    GUNZ ARE DONE!
    Shotgun pellets, automatic rifles, accuracy on right click lowering dispersion range for both shotgun and rifle.. Other shit probs too. Implementation for ammo system and decreasing the ammo count after shot is there. Need to implemenet a fire-rate system.

--- Platformer_Prototype/Assets/Scripts/WeaponHandler.cs
+++ Platformer_Prototype/Assets/Scripts/WeaponHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WeaponHandler : MonoBehaviour
 {
+    public int m_iShotgunPelletDispersionRange;
+    public int m_iRifleBulletDispersionRange;
     public static int m_iCurrentWeapon;
     private int m_iPlayerHeldShoot;
     public GameObject[] m_goWeapons;
@@ -14,6 +16,8 @@ public class WeaponHandler : MonoBehaviour
     {
         m_iPlayerHeldShoot = 0;
         m_iCurrentWeapon = 0;
+        m_iShotgunPelletDispersionRange = 35;
+        m_iRifleBulletDispersionRange = 10;
         for (int i = 0; i < 3; i++)
         {
             if (i != m_iCurrentWeapon)
@@ -69,65 +73,48 @@ public class WeaponHandler : MonoBehaviour
        // var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         Vector3 last_direction = new Vector3(0,0);
-      
-        
-        switch (WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_sWeaponName)
-        {
-            case "Pistol":
-                print("SHOOT PISTOL");
+        int _iWidth = Screen.width / 2;
+        int _iHeight = Screen.height / 2;
 
-                Debug.DrawRay(ray.origin, ray.direction * 1000, new Color(1f, 0.922f, 0.016f, 1f));
-                if (Physics.Raycast(ray.origin, ray.direction * 1000, out hit, 250.0f))
-                {
-                    print(hit.transform.name);
-                    CheckHit(hit);
-                }
-                break;
-            case "Shonny":
-                for (int i = 0; i < 9; i++) // random shotgun pellet variance
-                {
-                    bool shot = false;
-                    randomShotCunt = ray.direction;
-                    while (!shot)
+       // if(WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_iMagCount > 0)
+       // {
+            switch (WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_sWeaponName)
+            {
+                case "Pistol":
+                    ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+                    Debug.DrawRay(ray.origin, ray.direction * 1000, new Color(1f, 0.922f, 0.016f, 1f));
+                    if (Physics.Raycast(ray.origin, ray.direction * 1000, out hit, 250.0f))
                     {
-                        if (randomShotCunt != last_direction || randomShotCunt != ray.direction)
+                        print(hit.transform.name);
+                        CheckHit(hit);
+                    }
+                    break;
+                case "Shonny":
+                    for (int i = 0; i < 9; i++) // random shotgun pellet variance
+                    {
+                        ray = Camera.main.ScreenPointToRay(new Vector3(Random.Range(_iWidth - m_iShotgunPelletDispersionRange, _iWidth + m_iShotgunPelletDispersionRange),
+                            Random.Range(_iHeight - m_iShotgunPelletDispersionRange, _iHeight + m_iShotgunPelletDispersionRange)));
+                        Debug.DrawRay(ray.origin, ray.direction * 1000, new Color(1f, 0.922f, 0.016f, 1f));
+                        if (Physics.Raycast(ray.origin, ray.direction * 1000, out hit, 250.0f))
                         {
-                            Debug.DrawRay(ray.origin, randomShotCunt * 1000, new Color(1f, 0.922f, 0.016f, 1f));
-                            print("[" + i + "] Random Range: " + randomShotCunt);
-                            last_direction = randomShotCunt;
-                            if (Physics.Raycast(ray.origin, randomShotCunt * 1000, out hit, 250.0f))
-                            {
-                                print(hit.transform.name);
-                                CheckHit(hit);
-                            }
-                            shot = true;
-                        }
-                        else
-                        {
-                            randomShotCunt.x *= Random.Range(0.0f, 0.1f);
-                            print("[" + i + "] Random Range: " + randomShotCunt);
+                            print(hit.transform.name);
+                            CheckHit(hit);
                         }
                     }
-                    print("Firing shot: " + m_iPlayerHeldShoot);
-                    //randomShotCunt.x = Random.Range(0.0f, 0.05f);
-                    //randomShotCunt.y = Random.Range((Screen.width / 4 * 2), (Screen.width / 4 * 3));
-
-                   // randomShotCunt.z = Random.Range((Screen`.width / 4 * 2), (Screen.width / 4 * 3));
-                    Debug.DrawRay(ray.origin, randomShotCunt * 10000, new Color(1f, 0.922f, 0.016f, 1f));
-                }
-                break;
-            case "Bazzy":
-                for (int i = 0; i < WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_iMagCount; i++) // automatic weapon variance in shot..
-                {
-                    randomShotCunt = ray.direction;
-                    print("Firing shot: " + m_iPlayerHeldShoot);
-                  //  randomShotCunt.x = Random.Range(0.1f, 0.5f);
-               //     randomShotCunt.y = Random.Range(0.1f, 0.5f);
-                   // randomShotCunt.z = 0.0f;
-                    Debug.DrawRay(ray.origin, (randomShotCunt * -1) * 10000, new Color(1f, 0.922f, 0.016f, 1f));
-                }
-                break;
-        }
+                    break;
+                case "Rifle":
+                    ray = Camera.main.ScreenPointToRay(new Vector3(Random.Range(_iWidth - m_iRifleBulletDispersionRange, _iWidth + m_iRifleBulletDispersionRange),
+                           Random.Range(_iHeight - m_iRifleBulletDispersionRange, _iHeight + m_iRifleBulletDispersionRange)));
+                    Debug.DrawRay(ray.origin, ray.direction * 1000, new Color(1f, 0.922f, 0.016f, 1f));
+                    if (Physics.Raycast(ray.origin, ray.direction * 1000, out hit, 250.0f))
+                    {
+                        print(hit.transform.name);
+                        CheckHit(hit);
+                    }
+                    break;
+            }
+           // WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_iMagCount--;
+       // }
     }
 
     // Update is called once per frame
@@ -161,14 +148,49 @@ public class WeaponHandler : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Mouse0)) // player shooting
         {
-            GenerateGunShot();
+            if (WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_sWeaponName != "Rifle")
+            {
+                GenerateGunShot();
+            }
         }
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            if (m_iPlayerHeldShoot < WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_iMagCount)
+           if (WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_sWeaponName == "Rifle")
             {
-                m_iPlayerHeldShoot++;
+                GenerateGunShot();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            switch(WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_sWeaponName)
+            {
+                case "Pistol":
+                    break;
+                case "Shonny":
+                    m_iShotgunPelletDispersionRange = 17;
+                    break;
+                case "Rifle":
+                    m_iRifleBulletDispersionRange = 5;
+                    break;
+
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1)) // player shooting
+        {
+            switch (WeaponHandler.m_gActiveWeapon.GetComponent<WeaponStats>().m_sWeaponName)
+            {
+                case "Pistol":
+                    break;
+                case "Shonny":
+                    m_iShotgunPelletDispersionRange = 35;
+                    break;
+                case "Rifle":
+                    m_iRifleBulletDispersionRange = 10;
+                    break;
+
             }
         }
 
