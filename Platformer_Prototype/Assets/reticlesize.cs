dc35f7885d8ef5d2471cee04eb1880b8f5ca802d
BlairCorban <blaircorban@hotmail.com>
2018-08-02 19:01:45 +1200

    cursor

--- Platformer_Prototype/Assets/reticlesize.cs
+++ Platformer_Prototype/Assets/reticlesize.cs
@@ -6,14 +6,17 @@ public class reticlesize : MonoBehaviour {
     public RectTransform tr;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Mouse1))
         {
-            
+            transform.localScale = new Vector3(transform.localScale.x * 0.5f, transform.localScale.y * 0.5f, transform.localScale.z);
         }
-	}
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            transform.localScale = new Vector3(transform.localScale.x * 2, transform.localScale.y * 2, transform.localScale.z);
+        }
+    }
 }
